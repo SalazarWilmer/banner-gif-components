c9b52e9aa79239918ccbd35d1b524f720dde8e2f
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-11-27 13:53:52 +0100

    fix run dev

--- componentes-01/src/App.tsx
+++ componentes-01/src/App.tsx
@@ -1,14 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import "./style/App.scss";
-import { HomePage } from "./pages/home/HomePage";
-// import { HomePageData } from '../src/data/HomePageData.mock'
 
 function App() {
   return (
     <div className="Principal">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Routes></Routes>
     </div>
   );
 }
