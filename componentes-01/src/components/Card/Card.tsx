4984201d21f839829054edc0526c5a6c97c65451
Wilmer Salazar <wilmersalazarlara@gmail.com>
2023-11-27 15:11:45 +0100

    refactor: scss

--- componentes-01/src/components/Card/Card.tsx
+++ componentes-01/src/components/Card/Card.tsx
@@ -1,40 +1,45 @@
+  import React, { useState, useEffect } from "react";
+  import styles from "./Card.module.scss"; 
 
-import React, { useState, useEffect } from "react";
+  interface CardProps {
+    images: string[]; 
+  }
 
-interface CardProps {
-  images: string[]; 
-}
+  const Card: React.FC<CardProps> = ({ images }) => {
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-const Card: React.FC<CardProps> = ({ images }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    let interval: NodeJS.Timeout;
-    if (isFavorite && images.length > 1) {
-      interval = setInterval(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-      }, 1000); 
-    }
-    return () => {
-      if (interval) {
-        clearInterval(interval);
+    useEffect(() => {
+      let interval: NodeJS.Timeout;
+      if (isFavorite && images.length > 1) {
+        interval = setInterval(() => {
+          setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 1000);
       }
-    };
-  }, [isFavorite, images.length]);
+      return () => {
+        if (interval) {
+          clearInterval(interval);
+        }
+      };
+    }, [isFavorite, images.length]);
 
-  const toggleFavorite = () => {
-    setIsFavorite(!isFavorite);
+    const toggleFavorite = () => {
+      setIsFavorite(!isFavorite);
+    };
+
+    
+    const cardClassName = isFavorite ? styles.favoriteCard : styles.card;
+
+    return (
+      <div className={styles.container}>
+      <div className={cardClassName}>
+        <img className={isFavorite ? styles.isFavorite : styles.imgGif} src={images[currentImageIndex]} alt="Avatar" />
+        <button className={styles.buttonFav} onClick={toggleFavorite}>
+          {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+        </button>
+      </div>
+      </div>
+    );
   };
 
-  return (
-    <div>
-      <img src={images[currentImageIndex]} alt="Avatar" />
-      <button onClick={toggleFavorite}>
-        {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
-      </button>
-    </div>
-  );
-};
-
-export default Card;
+  export default Card;
